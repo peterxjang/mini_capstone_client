514c3f37c2e46fc031a22266c5a1774395a0f91a
Peter Jang <peter.x.jang@gmail.com>
2021-03-10 20:17:29 -0600

    Add authorization for orders and products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_11_020527) do
+ActiveRecord::Schema.define(version: 2021_03_11_021539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_03_11_020527) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
   end
 
 end
